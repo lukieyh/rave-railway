d183d41387edf615a4380e1b4780ac65e0b0d2d5
lukieyh <lukiey.git@gmail.com>
2024-10-17 00:06:55 -0300

    add - pages (front-end)

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home');
 });
+Route::get('/login', function () {
+    return view('pages.login');
+});
+Route::get('/register', function () {
+    return view('pages.registro');
+});
+Route::get('/marketplace', function () {
+    return view('pages.marketplace');
+});
+Route::get('/pagina-produto', function () {
+    return view('pages.paginaproduto');
+});
+Route::get('/usuario', function () {
+    return view('pages.paginausuario');
+});
+Route::get('/criacao-produto', function () {
+    return view('pages.criacaoproduto');
+});
+
